a603e5ddca90baf191e90256e7196117873c0bea
Suyun-Kim <61129726+Suyun-Kim@users.noreply.github.com>
2023-02-09 15:53:16 +0900

    리뉴얼 작업중.

--- src/main/java/kr/co/ares/config/WebConfig.java
+++ src/main/java/kr/co/ares/config/WebConfig.java
@@ -1,9 +1,11 @@
 package kr.co.ares.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Slf4j
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
--- src/main/java/kr/co/ares/config/filter/CorsFilter.java
+++ src/main/java/kr/co/ares/config/filter/CorsFilter.java
@@ -19,7 +19,7 @@ public class CorsFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
 
-        response.setHeader("Access-Control-Allow-Origin", "http://18.181.246.166");
+        response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods","*");
         response.setHeader("Access-Control-Max-Age", "3600");
